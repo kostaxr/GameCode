b21669e305789698a72805612dbd5b0e9d2dc0af
kostaxr <kostaxr@gmail.com>
2022-04-24 14:42:45 +0300

    added enemy targeting

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -5,30 +5,37 @@ public class Enemy : MonoBehaviour
 {
    private int health = 100;
    private float speed = 100f;
-
-   
+  
    AudioSource audioSource;
+   private Transform player;
+   void Awake()
+   {
+       transform.position = new Vector3(0.0f,0.0f,0.0f);
+   }
 
+   void Start()
+   {
+      audioSource = GetComponent<AudioSource>();
+      gameObject.tag = "Enemy";
+   }
+   private void Update()
+   {
+     if (health <= 0)
+     {
+       Destroy(this.gameObject);
+     }
+     if (Vector3.Distance(transform.position,player.position)> 1.0f)
+     {   
+         var distance = speed*Time.deltaTime; 
+         transform.position = Vector3.MoveTowards(transform.position,player.position,distance);
+     }
+   }
 
-    void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-        gameObject.tag = "Enemy";
-
-    }
-    private void Update()
-    {
-        if (health <= 0)
-        {
-            Destroy(this.gameObject);
-        }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-       if (collision.gameObject.tag == "Log")
-       {
-           health -= 30;
-       }
-    }
+   void OnCollisionEnter(Collision collision)
+   {
+      if (collision.gameObject.tag == "Log")
+      {
+        health -= 30;
+      }
+   }
 }
